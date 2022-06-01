96ff6d2cc1491a903b20d435215e018ae874a0d2
wwalpha <wwalpha@gmail.com>
2022-06-01 23:48:57 +0900

    update

--- src/24.S02.ts
+++ src/24.S02.ts
@@ -57,71 +57,71 @@ const readFile = (filename: string): string[] => {
 };
 
 const start = () => {
-  // const result: string[][] = [
-  //   readFile('S02E01.ass'),
-  //   readFile('S02E02.ass'),
-  //   readFile('S02E03.ass'),
-  //   readFile('S02E04.ass'),
-  //   readFile('S02E05.ass'),
-  //   readFile('S02E06.ass'),
-  //   readFile('S02E07.ass'),
-  //   readFile('S02E08.ass'),
-  //   readFile('S02E09.ass'),
-  //   readFile('S02E10.ass'),
-  //   readFile('S02E11.ass'),
-  //   readFile('S02E12.ass'),
-  //   readFile('S02E13.ass'),
-  //   readFile('S02E14.ass'),
-  //   readFile('S02E15.ass'),
-  //   readFile('S02E16.ass'),
-  //   readFile('S02E17.ass'),
-  //   readFile('S02E18.ass'),
-  //   readFile('S02E19.ass'),
-  //   readFile('S02E20.ass'),
-  //   readFile('S02E21.ass'),
-  //   readFile('S02E22.ass'),
-  //   readFile('S02E23.ass'),
-  //   readFile('S02E24.ass'),
-  // ];
-
-  const result: string[] = [
-    ...readFile('S02E01.ass'),
-    ...readFile('S02E02.ass'),
-    ...readFile('S02E03.ass'),
-    ...readFile('S02E04.ass'),
-    ...readFile('S02E05.ass'),
-    ...readFile('S02E06.ass'),
-    ...readFile('S02E07.ass'),
-    ...readFile('S02E08.ass'),
-    ...readFile('S02E09.ass'),
-    ...readFile('S02E10.ass'),
-    ...readFile('S02E11.ass'),
-    ...readFile('S02E12.ass'),
-    ...readFile('S02E13.ass'),
-    ...readFile('S02E14.ass'),
-    ...readFile('S02E15.ass'),
-    ...readFile('S02E16.ass'),
-    ...readFile('S02E17.ass'),
-    ...readFile('S02E18.ass'),
-    ...readFile('S02E19.ass'),
-    ...readFile('S02E20.ass'),
-    ...readFile('S02E21.ass'),
-    ...readFile('S02E22.ass'),
-    ...readFile('S02E23.ass'),
-    ...readFile('S02E24.ass'),
+  const result: string[][] = [
+    readFile('S02E01.ass'),
+    readFile('S02E02.ass'),
+    readFile('S02E03.ass'),
+    readFile('S02E04.ass'),
+    readFile('S02E05.ass'),
+    readFile('S02E06.ass'),
+    readFile('S02E07.ass'),
+    readFile('S02E08.ass'),
+    readFile('S02E09.ass'),
+    readFile('S02E10.ass'),
+    readFile('S02E11.ass'),
+    readFile('S02E12.ass'),
+    readFile('S02E13.ass'),
+    readFile('S02E14.ass'),
+    readFile('S02E15.ass'),
+    readFile('S02E16.ass'),
+    readFile('S02E17.ass'),
+    readFile('S02E18.ass'),
+    readFile('S02E19.ass'),
+    readFile('S02E20.ass'),
+    readFile('S02E21.ass'),
+    readFile('S02E22.ass'),
+    readFile('S02E23.ass'),
+    readFile('S02E24.ass'),
   ];
 
-  // result.forEach((item, idx) => {
-  //   fs.writeFileSync(`./results/24/Season2/S02E${(idx + 1).toString().padStart(2, '0')}.txt`, item.join('\n'), {
-  //     flag: 'w',
-  //   });
-  // });
+  // const result: string[] = [
+  //   ...readFile('S02E01.ass'),
+  //   ...readFile('S02E02.ass'),
+  //   ...readFile('S02E03.ass'),
+  //   ...readFile('S02E04.ass'),
+  //   ...readFile('S02E05.ass'),
+  //   ...readFile('S02E06.ass'),
+  //   ...readFile('S02E07.ass'),
+  //   ...readFile('S02E08.ass'),
+  //   ...readFile('S02E09.ass'),
+  //   ...readFile('S02E10.ass'),
+  //   ...readFile('S02E11.ass'),
+  //   ...readFile('S02E12.ass'),
+  //   ...readFile('S02E13.ass'),
+  //   ...readFile('S02E14.ass'),
+  //   ...readFile('S02E15.ass'),
+  //   ...readFile('S02E16.ass'),
+  //   ...readFile('S02E17.ass'),
+  //   ...readFile('S02E18.ass'),
+  //   ...readFile('S02E19.ass'),
+  //   ...readFile('S02E20.ass'),
+  //   ...readFile('S02E21.ass'),
+  //   ...readFile('S02E22.ass'),
+  //   ...readFile('S02E23.ass'),
+  //   ...readFile('S02E24.ass'),
+  // ];
 
-  const array = _.orderBy(Array.from(new Set(result)));
-
-  fs.writeFileSync(`./results/24/Season2/S02E99.txt`, array.join('\n'), {
-    flag: 'w',
+  result.forEach((item, idx) => {
+    fs.writeFileSync(`./results/24/Season2/S02E${(idx + 1).toString().padStart(2, '0')}.txt`, item.join('\n'), {
+      flag: 'w',
+    });
   });
+
+  // const array = _.orderBy(Array.from(new Set(result)));
+
+  // fs.writeFileSync(`./results/24/Season2/S02E99.txt`, array.join('\n'), {
+  //   flag: 'w',
+  // });
 };
 
 start();
